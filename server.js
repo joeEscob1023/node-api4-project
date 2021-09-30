77fd605837ca1936c43976d0dbed76b2893d82b1
Joe Escobedo <joe_escob1023@live.com>
2021-09-30 14:19:57 -0700

    3 request points work

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ server.get("/users", async (req, res) => {
   //User.find();
   try {
     const users = await User.find();
+    console.log(users);
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({
@@ -36,8 +37,19 @@ server.post("/api/register", async (req, res) => {
   }
 });
 
-server.post("/login", (req, res) => {
-  console.log("test");
+server.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  try {
+    if (!req.body.username || !req.body.password) {
+      res.status(400).json({
+        message: "Please provide correct username and password for the user",
+      });
+    } else {
+      res.send(`<h1> Hello there, ${req.body.username}!</h2>`);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "There was an error finding user" });
+  }
 });
 
 module.exports = server;
